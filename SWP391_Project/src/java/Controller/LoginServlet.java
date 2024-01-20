a63b659d077c7066058d89f8808e813f9da2d22c
NgcHuy <huynnhe171957@fpt.edu.vn>
2024-01-21 05:40:54 +0700

    da xong login ajax

--- SWP391_Project/src/java/Controller/LoginServlet.java
+++ SWP391_Project/src/java/Controller/LoginServlet.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller;
 
 import Entity.User;
+import Validate.validate;
 import dao.DAO;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,34 +20,37 @@ import jakarta.servlet.http.HttpSession;
  * @author tudo7
  */
 public class LoginServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");  
+            out.println("<title>Servlet LoginServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -55,12 +58,13 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -68,46 +72,74 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         //processRequest(request, response);
         String user = request.getParameter("user");
-        String pass = request.getParameter("password");     
-         request.setAttribute("username", user);
-         request.setAttribute("pass", pass);
+        String pass = request.getParameter("password");
+        request.setAttribute("username", user);
+        request.setAttribute("pass", pass);
         String captcha = request.getParameter("capchaRespone");
-          String sessionCaptcha = (String) request.getSession().getAttribute("captcha");
-          DAO dal = new DAO();
+        String sessionCaptcha = (String) request.getSession().getAttribute("captcha");
+        DAO dal = new DAO();
         User us = dal.Login(user, pass);
-          if(captcha!=null && captcha.equals(sessionCaptcha)){
-              if(us==null){
-           
-            request.setAttribute("mess", "Wrong user or pass");
-            request.getRequestDispatcher("login.jsp").forward(request, response);  
-        }else if(us.isIs_Active()==false){
-            
-            request.setAttribute("mess", "Account has banned!");
-            request.getRequestDispatcher("login.jsp").forward(request, response);  
-        }else{
-             HttpSession session = request.getSession();
-            session.setAttribute("user", us);
-            response.sendRedirect("home.jsp");
+//        validate val = new validate();
+        try {
+            if (captcha != null && captcha.equals(sessionCaptcha)) {
+                if (user == null) {
+                    response.getWriter().write("Username cannot be empty!");
+                } else if (pass == null) {
+                    response.getWriter().write("Password cannot be empty!");
+                } else {
+                    if (us == null) {
+                        response.getWriter().write("ACCOUNT DOES NOT EXIT!");
+                    } else if (us.isIs_Active() == false) {
+                        response.getWriter().write("ACCOUNT HAS BANNED!");
+                    } else {
+                        HttpSession session = request.getSession();
+                        session.setAttribute("user", us);
+                        response.getWriter().write("success");
+                        
+                    }
+                }
+
+            }else if(captcha.isEmpty()){
+                response.getWriter().write("Captcha cannot be empty!");
+            }else{
+                response.getWriter().write("Captcha wrong!");
+            }
+
+            }
+//            if(val.checkInput(pass, "\"^(?=.*[!@#$%^&*(),.?\\\":{}|<>]).*$\"", 3, 15))
+         catch (Exception e) {
+            System.out.println("Error");
         }
-          }
-          else if(captcha.equals("")){
-              request.setAttribute("mess", "Captcha cannot be left blank!");
-            request.getRequestDispatcher("login.jsp").forward(request, response);  
-          }else{
-            request.setAttribute("mess", "Captcha is wrong!");
-            request.getRequestDispatcher("login.jsp").forward(request, response);  
-          }
-        
-        
-        
-        
+
+//          if(captcha!=null && captcha.equals(sessionCaptcha)){
+//              if(us==null){
+//            request.setAttribute("mess", "Wrong user or pass");
+//            request.getRequestDispatcher("login.jsp").forward(request, response);  
+//        }else if(us.isIs_Active()==false){
+//            
+//            request.setAttribute("mess", "Account has banned!");
+//            request.getRequestDispatcher("login.jsp").forward(request, response);  
+//        }else{
+//             HttpSession session = request.getSession();
+//            session.setAttribute("user", us);
+//            response.sendRedirect("home.jsp");
+//        }
+//          }
+//          else if(captcha.equals("")){
+//              request.setAttribute("mess", "Captcha cannot be left blank!");
+//            request.getRequestDispatcher("login.jsp").forward(request, response);  
+//          }else{
+//            request.setAttribute("mess", "Captcha is wrong!");
+//            request.getRequestDispatcher("login.jsp").forward(request, response);  
+//          }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
--- SWP391_Project/web/login.jsp
+++ SWP391_Project/web/login.jsp
@@ -15,27 +15,14 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        <script>
-            function showPass(){
-            var passwordfield = document.getElementById("password");
-            var icon = document.querySelector(".show-password");
-            if (passwordfield.type = "password") {
-                    passwordfield.type ="text";
-                    icon.innerHTML = '<i class="fa fa-eye"></i>';
-                } else {
-                    passwordfield.type = "password";
-                    icon.innerHTML = '<i class="fa fa-eye-slash"></i>';
-}
-            }
-        </script>
+        
         <div class="cont" style="height: 600px">
             <div class="form sign-in">
                 <h2>Welcome</h2>
-                <form action="login" method="post">
-                    <div style="color: red;size: 10px;text-align: center">${mess}</div>
+                <form>
                     <label>
                         <span>User</span>
-                        <input type="text" name="user" value="${username}"/>
+                        <input id="user" type="text" name="user" value="${username}"/>
                     </label>
                     <label>
                         <span>Password</span>
@@ -48,16 +35,16 @@
                         
                         <div class="input-group-prepend" style="display: flex">
                             <img id="captchaImage" src="captchaimage" alt="CAPTCHA image" />
-                            <input type="text" placeholder="Captcha" name="capchaRespone" />
+                            <input id="captcha" type="text" placeholder="Captcha" name="capchaRespone" />
                             <button type="button" onclick="refreshCaptcha()" class="btn btn-light" style="width: 15%"><i class="fa fa-refresh" style="color: black;"></i></button>
                            </div>
                     </label>
                     
                     <p class="forgot-pass">Forgot password?</p>
 
-                    <button type="submit" class="submit">Sign In</button>
+                    <button onclick="SignIn()" type="button" class="submit">Sign In</button>
                 </form>
-                <button type="button" class="submit" style="background-color: gray" id="homeButton">Home Page</button>
+                <button type="button" class="submit" style="background-color: gray" id="homeButton" >Home Page</button>
                 <button type="button" class="submit" style="background-color: gray" > <a href="https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:9999/SWP391_Project/loginGoogleHandler&response_type=code&client_id=351960657846-lejfsvfrmte5906hvkrj70out2u775s0.apps.googleusercontent.com&approval_prompt=force" style="color: white; text-decoration: none;">Login with google</a></button>
 </div>
         
@@ -99,7 +86,7 @@
                                             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
            <script>
                                
-         $(document).ready(function () {
+                                                $(document).ready(function () {
                                                     $("#homeButton").click(function () {
                                                         $.ajax({
                                                             type: 'GET',
@@ -144,13 +131,50 @@
                                                         });
                                                     });
                                                 });
+                                                </script>
                                                 
-                                                function refreshCaptcha() {
+                                                
+ <script>        
+    function SignIn(){
+                   var username = $("#user").val();
+                   var password = $("#password").val();
+                   var captcha = $("#captcha").val();
+                   $.ajax({
+                       type:"POST",
+                       url:"login",
+                       data: {
+                           user : username,
+                           password : password,
+                           capchaRespone : captcha
+                       },
+                       success: function (response){
+                           if(response === "success"){
+                               window.location.href = "home.jsp";
+                           }else{
+                               alert(response);
+                           }
+                       }
+                      
+                       
+                   });
+               };                                 
+                function refreshCaptcha() {
                 // Sử dụng AJAX để làm mới ảnh CAPTCHA
                 $.get('refreshcaptcha', function () {
                     // Thay đổi src của ảnh để làm mới
                     $('#captchaImage').attr('src', 'captchaimage?' + new Date().getTime());
                 });
+            };
+            function showPass(){
+            var passwordfield = document.getElementById("password");
+            var icon = document.querySelector(".show-password");
+            if (passwordfield.type === "password") {
+                    passwordfield.type ="text";
+                    icon.innerHTML = '<i class="fa fa-eye"></i>';
+                } else {
+                    passwordfield.type = "password";
+                    icon.innerHTML = '<i class="fa fa-eye-slash"></i>';
+}
             }
                                             </script>
 <script>
